23017f3dbc5088d26eec89ca6e44d368ebd38e5b
Clone5030 <65713825+Clone5030@users.noreply.github.com>
2026-06-02 12:48:50 -0400

    Update GameWorld.java

--- GameWorld.java
+++ GameWorld.java
@@ -2,8 +2,19 @@ import greenfoot.*;
 
 public class GameWorld extends World
 {
+    int worldSize = 16;
+    int tileLength = 60;
+    int tileHeight = 40;
+    int[][] worldArray = new int[worldSize][worldSize];
+    
     public GameWorld()
     {
-        super(1200, 800, 1); 
+        super(1200, 800, 1);
+        setBackground();
+    }
+    
+    private void setBackground(){
+        GreenfootImage background = new GreenfootImage("background.png");
+        setBackground(background);
     }
 }
