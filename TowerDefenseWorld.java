88a35aa74033f5178c2f149df8c31dd4e8c6ee83
ivan440013993 <440013993@gapps.yrdsb.ca>
2026-06-02 12:34:44 -0400

    updated world class to have set background and to generate a 2d array that populates the paths

---
+++ TowerDefenseWorld.java
@@ -0,0 +1,50 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Main Game World
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TowerDefenseWorld extends World
+{
+    private GreenfootImage background;
+    
+    int[][] gameArray = new int[16][16];
+    
+    public TowerDefenseWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(1200, 800, 1); 
+        
+        setBackground();
+        
+        for (int i = 0; i < 0; i++) {
+            for (int j = 0; j < 0; j++) {
+                gameArray[i][j] = 0;
+            }
+        }
+        
+        int randomStart = Greenfoot.getRandomNumber(31);
+        int randomEnd = Greenfoot.getRandomNumber(31);
+        
+        if (randomStart < 16) {
+            gameArray[0][randomStart] = 1;
+        } else {
+            randomStart -= 15;
+            gameArray[randomStart][0] = 1;
+        }
+        
+        if (randomEnd < 16) {
+            gameArray[15][randomEnd] = 1;
+        } else {
+            randomEnd -= 15;
+            gameArray[randomEnd][15] = 1;
+        }
+    }
+    
+    public void setBackground () {
+        background = new GreenfootImage("background.png");
+        setBackground(background);
+    }
+}
