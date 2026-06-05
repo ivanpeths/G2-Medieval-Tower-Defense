92bae332ca942f41d53d5a1cbbcddb2b26ed9658
ivan440013993 <440013993@gapps.yrdsb.ca>
2026-06-05 13:26:22 -0400

    temporary knight done

--- Knight.java
+++ Knight.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.List;
 
 /**
  * Write a description of class Knight here.
@@ -8,12 +9,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Knight extends Tower
 {
+    int radius = 50;
+    
     public Knight () {
         image = new GreenfootImage("knight.png");
         setImage(image);
     }
     
     protected void attack () {
-        
+        List<Enemy> enemies = getObjectsInRange(radius, Enemy.class);
+        for (Enemy enemy : enemies) {
+            enemy.hurt(dmg);
+        }
     }
 }
