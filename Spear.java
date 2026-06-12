06e7c5bd254eefed2e43044f52d868c2652cf54e
ivan440013993 <440013993@gapps.yrdsb.ca>
2026-06-12 13:13:00 -0400

    fixed spear being op

--- Spear.java
+++ Spear.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Spear extends Projectiles
 {
+    private int maxHits;
     public Spear (int angle, int damage) {
         image = new GreenfootImage("spear.png");
         image.scale(40, 4);
@@ -16,6 +17,7 @@ public class Spear extends Projectiles
         speed = Greenfoot.getRandomNumber(2) + 6;
         setRotation (angle);
         this.damage = damage;
+        maxHits = 3;
     }
 
     public void act()
@@ -28,6 +30,11 @@ public class Spear extends Projectiles
         Enemy target = (Enemy)getOneIntersectingObject(Enemy.class);
         if (target != null) {
             target.hurt(damage);
+            maxHits--;
+        }
+        
+        if (maxHits == 0) {
+            getWorld().removeObject(this);
         }
     }
 }
